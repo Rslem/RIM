e6ee0ab280637bb6f886a3377d50be98b4c9b65e
Rslem <32941151+Rslem@users.noreply.github.com>
2017-11-15 04:05:45 +0100

    Add files via upload

---
+++ Adventure.js
@@ -0,0 +1,15 @@
+
+// Created on 12/11/2017 23:00:24
+
+confirm("I understand!") {
+  
+}
+var age=prompt("What's your age?");
+if(age is >= 10)
+{
+  console.log("Unfortunately, you are too young to play :(");
+}
+else
+{
+  console.log("Welcome! Let's continue!");
+  
